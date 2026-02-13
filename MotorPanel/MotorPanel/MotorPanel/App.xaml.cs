f2c95a6d6800e2c875d52d07dc9631378a76cef9
Simon Hausberger <simohausberger@gmail.com>
2026-02-13 14:57:41 +0100

    zwischenstand: leds und anzeigen fehlen noch, bis jetzt nur grafisch (XAML)

--- MotorPanel/MotorPanel/MotorPanel/App.xaml.cs
+++ MotorPanel/MotorPanel/MotorPanel/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using Syncfusion.Licensing;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,11 @@ namespace MotorPanel
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            //SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1JGaF1cXmhKYVJyWmFZfVhgd19CZFZQR2Y/P1ZhSXxVdkdiWn9bdXFRQWhdUUd9XEA=");    //just 30 days
+            SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjGyl/VkR+XU9Ff1RBQmJNYVF2R2VJfFR0cF9HY0wxOX1dQl9lSXxSdUVrXX9fd3RdR2dXUkY=");          //student license
+        }
     }
 
 }
